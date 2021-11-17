9ca0c5b06602c24f390abc34739930b631bc895b
--replace-all <venkateshchowdary117@gmail.com>
2021-11-17 15:18:46 +0530

    added optimization using binary search for finding index  with O(logn)

--- arrays/sourcecode/searchInsert.py
+++ arrays/sourcecode/searchInsert.py
@@ -11,4 +11,22 @@ class Solution:
                 if target > value and target < nums[id+1]:
                     return id+1
         return len(nums)
+
+    def searchInsert(self,nums:List[int],target:int) -> int:
+        start:int=0
+        end:int=len(nums)
+        
+        if target <= nums[0]:
+            return 0
+        while(start<end):
+            mid:int=(start+end)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid] < target:
+                start=mid+1
+            else:
+                end=mid
+        return start
+
+
                 

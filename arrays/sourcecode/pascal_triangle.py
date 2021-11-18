7b691327da23bd5f95c4a9606d3ab4a0abe8d574
--replace-all <venkateshchowdary117@gmail.com>
2021-11-18 12:54:45 +0530

    adding optimized code for generating pascal triangle

--- arrays/sourcecode/pascal_triangle.py
+++ arrays/sourcecode/pascal_triangle.py
@@ -15,3 +15,12 @@ class solution:
                 temp+=1
             output.append([1]+local_output+[1])
         return output
+
+    def generate_optimized(self, numRows: int) -> List[List[int]]:
+        dp=[[1 for i in range(j+1)] for j in range(numRows)]
+        
+        for i in range(2,len(dp)):
+            for j in range(1,len(dp[i])-1):
+                dp[i][j]=dp[i-1][j]+dp[i-1][j-1]
+        
+        return dp
